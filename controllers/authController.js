09d1d3df28422309903512df17ab581fbe149456
unknown <100022225+om-satturwar61@users.noreply.github.com>
2024-10-12 01:18:10 +0530

    Further Modularization

---
+++ controllers/authController.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const userdata = require('../db/users');
+
+const loginHandler = (req, res) => {
+    const {username, password} = req.body;
+    console.log({username, password});
+    const isUserVerified = userdata.users.some(user => user.username === username && user.password === password)
+    if (isUserVerified){
+        const token = jwt.sign({id: username}, process.env.SECRET_KEY);
+        res.json({username, token, message: "User Verified"});
+    }
+    else{
+        res.status(401).json({message: "Invalid Credentials"});
+    }
+}
+
+module.exports = loginHandler;
--- router/auth.router.js
+++ router/auth.router.js
@@ -1,20 +1,8 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const userdata = require('../db/users');
+const loginHandler = require('../controllers/authController');
 
 const loginRouter = express.Router();
 
-loginRouter.route('/').post((req, res) => {
-    const {username, password} = req.body;
-    console.log({username, password});
-    const isUserVerified = userdata.users.some(user => user.username === username && user.password === password)
-    if (isUserVerified){
-        const token = jwt.sign({id: username}, process.env.SECRET_KEY);
-        res.json({username, token, message: "User Verified"});
-    }
-    else{
-        res.status(401).json({message: "Invalid Credentials"});
-    }
-});
+loginRouter.route('/').post(loginHandler);
 
 module.exports = loginRouter;
